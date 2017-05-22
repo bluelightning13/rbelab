32c562dac59d82938253706a313e1fd314f73844
bluelightning13 <mehiatt@wpi.edu>
2017-05-22 16:06:18 -0400

    rosdept order changes
    makes the kinect drivers install properly. need to run rosdep after those are installed or kinect may not be detected.

--- ROS_TURTLE_KINECT_INSTALL.sh
+++ ROS_TURTLE_KINECT_INSTALL.sh
@@ -12,39 +12,22 @@ sudo rosdep init
 
 rosdep update
 
-echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
-source ~/.bashrc
+
 
 sudo apt-get -y install python-rosinstall
-echo Ensuring base package are installed
+echo 'Ensuring base package are installed'
 sudo apt-get -y install ros-kinetic-ros-base 
-echo Ensuring gazebo plugins are installed
+echo 'Ensuring gazebo plugins are installed'
 sudo apt-get -y install ros-kinetic-gazebo-plugins
-echo Ensuring navigation packages are installed
+echo 'Ensuring navigation packages are installed'
 sudo apt-get -y install ros-kinetic-navigation
 
-echo Ensuring needed packages were installed
+echo 'Ensuring needed packages were installed'
 sudo apt-get -y install ros-kinetic-turtlebot-gazebo ros-kinetic-turtlebot-rviz-launchers  ros-kinetic-warehouse-ros  
 
-echo Now installing some extra packages
+echo 'Now installing some extra packages'
 sudo apt-get -y install  ros-kinetic-gazebo-ros ros-kinetic-gazebo-plugins ros-kinetic-gazebo-ros-control python-rosdep python-wstool ros-kinetic-ros ros-kinetic-warehouse-ros libspnav-dev 
 
-sudo rosdep init
-
-rosdep update
-
-
-sudo apt-get install libfreenect-dev
-
-sudo apt-get install ros-kinetic-freenect-launch
-
-mkdir ~/kinectdriver 
-cd ~/kinectdriver 
-git clone https://github.com/avin2/SensorKinect 
-cd SensorKinect/Bin/
-tar xvjf SensorKinect093-Bin-Linux-x64-v5.1.2.1.tar.bz2
-cd Sensor-Bin-Linux-x64-v5.1.2.1/
-sudo ./install.sh
 
 
 
@@ -62,5 +45,22 @@ echo "export TURTLEBOT_STAGE_WORLD_FILE=/maps/stage/maze.world" >> .bashrc
 echo "export TURTLEBOT_BATTERY=/sys/class/power_supply/BAT0" >> .bashrc
 echo "export TURTLEBOT_NAME=turtlebot" >> .bashrc
 
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+sudo apt-get install libfreenect-dev
+
+sudo apt-get install ros-kinetic-freenect-launch
+
+mkdir ~/kinectdriver 
+cd ~/kinectdriver 
+git clone https://github.com/avin2/SensorKinect 
+cd SensorKinect/Bin/
+tar xvjf SensorKinect093-Bin-Linux-x64-v5.1.2.1.tar.bz2
+cd Sensor-Bin-Linux-x64-v5.1.2.1/
+sudo ./install.sh
 
 
+sudo rosdep init
+
+rosdep update
